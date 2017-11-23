dd7385e87e128031e08044e643c24df264a68268
User <User@144.35.96.30>
2017-11-22 19:27:03 -0700

    Hola

--- BattleShipProject/src/battleShip/battleshipApp.java
+++ BattleShipProject/src/battleShip/battleshipApp.java
@@ -3,7 +3,7 @@ package battleShip;
 import java.util.Scanner;
 
 public class battleshipApp {
-//testing now lol
+
 	public static void main(String[] args) {
 	
 	    Scanner input = new Scanner (System.in);
