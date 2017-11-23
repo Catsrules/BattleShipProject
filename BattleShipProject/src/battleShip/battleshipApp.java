3bdb4fa290e3835375cf51e54c578cc01c647ba2
aperry <aperry@APERRY-PC1.svwater.com>
2017-11-22 17:08:21 -0700

    Added comments about testing BattleshipApp

--- BattleShipProject/src/battleShip/battleshipApp.java
+++ BattleShipProject/src/battleShip/battleshipApp.java
@@ -1,5 +1,5 @@
 package battleShip;
-//So how are you doing? 
+ 
 import java.util.Scanner;
 
 public class battleshipApp {
@@ -34,18 +34,20 @@ public class battleshipApp {
 	}
 	private static void singlePlayer()
 	{
+		//There needs to be instructions added to the user knows what to do. 
+		//There also needs to be inputs for the user to setup the game and play it. 
 	   Scanner input = new Scanner (System.in);
 	   BattleShip p1 = new BattleShip();
-	   if (!p1.addShipP1(1, 2, true, 2)) {
+	   if (!p1.addShipP1(1, 2, true, 2)) {//Testing adding ships We need to get it so it is all user input. For example 1 ,2 would need to be 2C. To start the ship and True is saying it IS vertical
+		   System.out.print("Bad location");//It addShipP1 returns false it will display "Bad Location" In need to make this a loop. So it asks the user again for a correct location.  
+	   }
+	   if (!p1.addShipP1(0, 1, false, 4)) {//Testing adding a second ship. 
 		   System.out.print("Bad location");
 	   }
-	   if (!p1.addShipP1(0, 1, false, 4)) {
+	   if (!p1.addShipP1(0, 0, true, 3)) {//Testing Adding a third ship that interfears with the second ship. (This should be a bad location.)  
 		   System.out.print("Bad location");
 	   }
-	   if (!p1.addShipP1(0, 0, true, 3)) {
-		   System.out.print("Bad location");
-	   }
-	   p1.player1Display();
+	   p1.player1Display();//This prints player1 and player1's view of players2's board.  
 	}
 	private static void multiPlayer()
 	{
